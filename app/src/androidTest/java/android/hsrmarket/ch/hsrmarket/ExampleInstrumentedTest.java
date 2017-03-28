49c5e125190be492a17d63cbd49266497f168983
Urs Forrer <mail@ursforrer.ch>
2017-03-28 17:31:15 +0200

    Everything is changing
    Everything is changing

--- app/src/androidTest/java/android/hsrmarket/ch/hsrmarket/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/android/hsrmarket/ch/hsrmarket/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ch.hsrmarket.android;
+package android.hsrmarket.ch.hsrmarket;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ch.hsrmarket.android", appContext.getPackageName());
+        assertEquals("android.hsrmarket.ch.hsrmarket", appContext.getPackageName());
     }
 }
--- app/src/main/java/android/hsrmarket/ch/hsrmarket/MainActivity.java
+++ app/src/main/java/android/hsrmarket/ch/hsrmarket/MainActivity.java
@@ -1,4 +1,4 @@
-package ch.hsrmarket.android;
+package android.hsrmarket.ch.hsrmarket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
